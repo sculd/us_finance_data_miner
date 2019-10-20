c7b6a0820c1e687c7ad5c95e0fe7a7ad9eaef7c2
Jun Lim <hjunlim@google.com>
2019-10-20 00:38:01 -0700

    adding etrade api imple for test

--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,19 @@
 import ingest.intraday.polygon
 
-ingest.intraday.polygon.download_histories_csv('2019-10-16')
+# ingest.intraday.polygon.download_histories_csv('2019-10-16')
 
 
 
 
+
+'''
+import util.auth_etrade
+
+session = util.auth_etrade.get_session()
+
+
+import ingest.intraday.etrade
+
+market = ingest.intraday.etrade.Market(session)
+market.quotes()
+'''
