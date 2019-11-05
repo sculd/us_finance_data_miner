a858fc9a8ef9fdc523a73491da503ecf54139551
Jun Lim <hjunlim@google.com>
2019-11-04 21:53:09 -0800

    better sleep schedule

--- run_daily_last.py
+++ run_daily_last.py
@@ -27,21 +27,22 @@ def run(forcerun):
         logging.info(cfg, 'checking if run for {dt_str} should be done'.format(dt_str=dt_str))
         if not forcerun and upload.daily.history.did_upload_today():
             logging.info(cfg, 'run for {dt_str} is already done'.format(dt_str=dt_str))
-            time.sleep(10 * 60)
+            time.sleep(30 * 60)
             continue
 
         t_run_after = config.get_daily_last_record_ingestion_start_t(cfg)
         while True:
-            t_cur = util.time.get_utcnow().astimezone(tz).time()
+            t_cur = util.time.get_now_time_tz()
             logging.info(cfg, 'checking if the schedule time for {dt_str} has reached'.format(dt_str=dt_str))
-            if forcerun or t_cur > t_run_after:
+            dt = util.time.time_diff_seconds(t_run_after, t_cur)
+            if forcerun or dt <= 0:
                 run_ingests()
                 run_upload()
                 upload.daily.history.on_upload()
                 break
 
             logging.info(cfg, 'schedule time {t_run_after} not yet reached at {t_cur}'.format(t_run_after=t_run_after, t_cur=t_cur))
-            time.sleep(10 * 60)
+            time.sleep(max(dt + 1, 60 * 60))
 
         if forcerun:
             # forcerun runs only once
--- util/time.py
+++ util/time.py
@@ -11,6 +11,16 @@ def get_now_tz():
     tz = config.get_tz(cfg)
     return get_utcnow().astimezone(tz)
 
+def get_now_time_tz():
+    '''
+    get datetime.time of now in korean time zone.
+    :return:
+    '''
+    cfg = config.load('config.us.yaml')
+    tz = config.get_tz(cfg)
+    now_tz = get_utcnow().astimezone(tz)
+    return datetime.time(now_tz.hour, now_tz.minute, now_tz.second, tzinfo=tz)
+
 def get_today_tz():
     '''
     :return: e.g: 2019-12-25
@@ -32,3 +42,16 @@ def get_today_v_tz():
     cfg = config.load('config.us.yaml')
     tz = config.get_tz(cfg)
     return get_utcnow().astimezone(tz).strftime('%Y%m%d')
+
+def time_diff_seconds(t1, t2):
+    '''
+    Get datetime.timedelta between two datetime.time
+
+    :param t1:
+    :param t2:
+    :return:
+    '''
+    today = datetime.date.today()
+    dt1, dt2 = datetime.datetime.combine(today, t1), datetime.datetime.combine(today, t2)
+    tf = dt1 - dt2
+    return tf.days * 24 * 3600 + tf.seconds
